c1eab6a21666cc356f7a88f85dae30f86bcf2797
DavidSer1 <davsermel@gmail.com>
2025-09-16 10:07:08 +0200

    practica3.1 iniciada

--- practica3.1.php
+++ practica3.1.php
@@ -1,3 +1,14 @@
 <?php 
 echo "hool";
+function mostar_impares(){
+    $frase = "A quien madruga Dios
+le ayuda";
+    $array_frase = explode(" ", $frase);
+    foreach($array_frase as $palabra){
+        if(strlen($palabra) % 2 != 0){
+            echo $palabra . "<br>";
+        }
+    }
+}
+
 ?>
