9fb589488dc5ba662b0689405595b2c22d63b795
DavidSer1 <davsermel@gmail.com>
2025-09-18 13:28:40 +0200

    mirar que pasa en el usuari

--- practica3.17/verificar.php
+++ practica3.17/verificar.php
@@ -14,7 +14,7 @@ echo "Todos los campos son obligatorios";
     foreach ($usuarios as $nombre => $password) {
         if ($usuario === $nombre && $contra === $password) {
           
-            fwrite($archivo, date("Y-m-d H:i:s") . " - Login correcto: $usuario\n");
+    /* fwrite($archivo, date("Y-m-d H:i:s") . " Usuario i contraseña incorrectes" . $usuario); */
            header("Location: ok.php");
         }
         else{
