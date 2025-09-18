541ad66a089ef9baeb73a664eeda49a5fa168d45
DavidSer1 <davsermel@gmail.com>
2025-09-18 10:59:39 +0200

    verificar ya funciona

--- practica3.17/verificar.php
+++ practica3.17/verificar.php
@@ -1,11 +1,19 @@
 <?php 
 include "usuarios.php";
-if($_SERVER["REQUEST_METHOD"]== "POST"){
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario =  trim($_POST["usuario"]);  
+    $contra  = $_POST["contra"] ?? '';
+    if($usuario == "" || $contra ==""){
+echo "Todos los campos son obligatorios";
+    }
 
+    foreach ($usuarios as $nombre => $password) {
+        
+        if ($usuario === $nombre && $contra === $password) {
+            echo "Login correcto: $usuario";
+        }
+       
+    }
 }
-
-
-
-
 ?>
